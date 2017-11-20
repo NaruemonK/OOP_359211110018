3f8735a5f293cd7a34faee5700e475d4d2105e3c
NaruemonK <tanmylove1995@gmail.com>
2017-11-20 15:45:07 +0700

    Variable and Data types

--- src/ooplab2/TestInputData.java
+++ src/ooplab2/TestInputData.java
@@ -1,7 +1,16 @@
 package ooplab2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestInputData {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.printf("What is your name? :");
+        String name = reader.readLine();
+        System.out.println("your name is "+name);
 
     }//main
 }//class
