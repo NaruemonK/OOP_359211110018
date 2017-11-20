c5495f0e476e8844291fb10e1deb9d65e03bbd17
NaruemonK <tanmylove1995@gmail.com>
2017-11-20 16:01:46 +0700

    Input Data

--- src/ooplab2/TestInputData.java
+++ src/ooplab2/TestInputData.java
@@ -3,6 +3,7 @@ package ooplab2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class TestInputData {
     public static void main(String[] args) throws IOException {
@@ -11,6 +12,15 @@ public class TestInputData {
         System.out.printf("What is your name? :");
         String name = reader.readLine();
         System.out.println("your name is "+name);
+        System.out.println("How old are you? :");
+        int age = Integer.parseInt(reader.readLine());
+        System.out.println("You are "+age+" Years old.");
+
+        //Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Whare are you?: ");
+        String address = scanner.nextLine();
+        System.out.println("You from: "+address);
 
     }//main
 }//class
