c7ab47d6ffafa6731934a3f2274de164f31afad9
NaruemonK <tanmylove1995@gmail.com>
2017-11-20 13:54:09 +0700

    Initial commit

---
+++ src/OOP_lab/s359211110018/rmutsv/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package OOP_lab.s359211110018.rmutsv.com;
+
+public class HelloWorld
+{
+    // Create main method
+    public static void main(String[] args){
+        System.out.println("Hello world, I'm Tan");
+        System.out.println("My name is Naruemon Kaewmee");
+        //add informetion
+        System.out.println("Faculty: Management Techonology,RMUTSV");
+    }//main
+}//class
