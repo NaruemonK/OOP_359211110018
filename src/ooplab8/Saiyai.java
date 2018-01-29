af408d3b705a1011b0d656526029d04c659bba46
NaruemonK <tanmylove1995@gmail.com>
2018-01-29 15:48:56 +0700

    Implements Campus

---
+++ src/ooplab8/Saiyai.java
@@ -0,0 +1,15 @@
+package ooplab8;
+
+public class Saiyai implements Campus{//เรียกใช้ Methoed 2ตัว
+
+
+    @Override
+    public void getCampusName() {
+        System.out.println("Saiyai Campus.");
+    }
+
+    @Override
+    public void getArae() {
+        System.out.println("Thungsong Nakhonsithammarat.");
+    }
+}
