b8c53253ffd9a8ec3b11a0b8023c22dfc8743378
LABCOM_6 <tanmylove1995@gmail.com>
2018-02-05 15:36:35 +0700

    PersonApp

--- src/ooplab9/PersonApp.java
+++ src/ooplab9/PersonApp.java
@@ -2,10 +2,21 @@ package ooplab9;
 
 public class PersonApp {
     public static void main(String[] args) {
-         Person person = new Person("Naruemon","359211110018",
-                 new Address("2/4","Trang","92160"),
-                 new Job("Student",20000));
+         Person person =
+                 new Person(
+                         "Naruemon",
+                         "359211110018",
+                 new Address(
+                         "2/4",
+                         "Trang",
+                         "92160"),
+                 new Job(
+                         "Student",
+                         20000));
          System.out.println(person.toString());
+         //ต้องการเปลี่ยนแค่เงินเดือนอย่างเดียว
+        person.getJob().setSalary(30000);
+        System.out.println(person.getJob());
 
 
     }
